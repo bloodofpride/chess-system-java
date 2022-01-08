36a532b43d1794fd73e2eec0229e066a60ed2814
bloodofpride <antonio_maxwell@hotmail.com>
2022-01-08 20:50:55 -0300

    Colocando peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,7 +33,10 @@ public class Board {
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
     }
-//    public void placePiece(Piece piece, Position position){}
+    public void placePiece(Piece piece, Position position){
+        this.pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 //    public void removePiece(Position position){}
 //    public boolean positionExists(Position position){}
 //    public boolean thereIsAPiece(Position position){}
